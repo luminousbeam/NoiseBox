12f25ae0341ae95284325df42400579144e73fd0
luminousbeam <jocelyn.jeriah@gmail.com>
2015-06-04 04:08:26 -0400

    i created a public file, css file and view files because i want to separate my file structure onto the node/express mvc. i have my index.html file currently showing up as my homepage on localhost 3000

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 var express = require("express")
 var app = express()
+var hbs = require("hbs")
+// var synth = require("../synth.js")
+// synth.play()
 
-app.get("/", function( req, res ){
-  res.send("hello world")
-})
+
+// app.set('view engine', 'hbs')
+// app.use(express.static(__dirname + '/public'))
+// hbs.registerPartials(__dirname+"/views/partials")
+//
+// app.get("/", function( req, res ){
+//   res.send("hello world")
+// })
+//
+// app.listen(3000, function(){
+//   console.log("app listening on port 3000")
+// })
+
+app.set('view engine', 'hbs')
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
 
 app.listen(3000, function(){
   console.log("app listening on port 3000")
