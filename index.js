b1eba0a8018ba14fb2a8122e87d08fc477bdef3b
luminousbeam <jocelyn.jeriah@gmail.com>
2015-06-06 17:08:22 -0400

    rearranged and deleted some code,still trying to deploy to heroku

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
-var express = require("express")
-var app = express()
-// var hbs = require("hbs")
+var express = require("express");
+var app = express();
+var hbs = require("hbs")
 app.set("view engine", "hbs");
-// hbs.registerPartials(__dirname+"/views/partials")
-//
-
-//WEBSOCKET
-// var http = require('http').Server(app);
-// var io = require('socket.io');
-// var io = io(http);
-//
-// io.on('connection', function(socket){
-//   socket.on("chatsend", function(message){
-//     io.emit("chatpost", message);
-//   });
-// });
-//
-
-
-//RETURNS INDEX.HTML W/O THE NEED OF HANDLEBARS OR MIDDLEWARE
-// app.get('/', function(req, res){
-//   res.sendFile(__dirname + '/public/index.html');
-// });
-
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function( req, res ){
-  res.render("index")
-})
-
+  res.render("index");
+});
 
 
 app.listen(3000, function(){
-  console.log("app listening on port 3000")
-})
+  console.log("app listening on port 3000");
+});
