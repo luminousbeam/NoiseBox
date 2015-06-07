7b320c77205ffde78e1ec8f44b9d51a89bda3b11
luminousbeam <jocelyn.jeriah@gmail.com>
2015-06-07 00:07:04 -0400

    i commented out the app.listen to port 3000 to see if this was causing issues with heroku deployment

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get("/", function( req, res ){
 });
 
 
-app.listen(3000, function(){
-  console.log("app listening on port 3000");
-});
+// app.listen(3000, function(){
+//   console.log("app listening on port 3000");
+// });
