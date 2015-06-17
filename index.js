858de87417febcf49a1b4d306abd66b03be878cf
luminousbeam <jocelyn.jeriah@gmail.com>
2015-06-17 12:23:09 -0400

    added components for chat using socket.io

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var hbs = require("hbs")
+
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + '/public'));
 
@@ -8,12 +11,18 @@ app.get("/", function( req, res ){
   res.render("index");
 });
 
+// Chat socket.io
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
 app.listen(3000, function(){
   console.log("app listening on port 3000");
 });
 
-
+// I added this to correct Heroku error 10
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
