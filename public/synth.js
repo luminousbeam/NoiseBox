c6b8eaae586a3acc768ad00ee1bbcef49ff58b93
luminousbeam <jocelyn.jeriah@gmail.com>
2015-06-17 12:42:57 -0400

    deleted chat.js folder and moved contents into synth.js folder

--- public/synth.js
+++ public/synth.js
@@ -64,3 +64,14 @@ var osc4 = document.getElementById("osc4");
   osc4.onmouseup = function() {
     oscillator.disconnect();
   };
+
+// CHAT
+var socket = io();
+  $('form').submit(function(){
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+  socket.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+  });
